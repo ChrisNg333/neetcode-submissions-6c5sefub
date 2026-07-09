f8cffdd3c1d9d53bc8621a7393c49e0e3c1dce82
Chris Nguyen <123434224+ChrisNg333@users.noreply.github.com>
2026-07-08 23:23:31 -0500

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,32 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+    def insert(self, word: str) -> None:
+        curr = self
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = PrefixTree()
+            curr = curr.children[ch]
+        curr.word = True        #upfdate the word   
+
+    def search(self, word: str) -> bool:
+        curr = self
+        for ch in word:
+            if ch not in curr.children:
+                return False        #exist early
+            curr = curr.children[ch]
+        
+        return curr.word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return True
+        
+        
